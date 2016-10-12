e7b70ae39975319c731819a10934ffd6129ce07a
Jeremy Venezia <veneziajeremy@gmail.com>
2016-10-12 11:37:13 +0200

    Add default rel on link_to with target blank

--- lib/safe_target_blank.rb
+++ lib/safe_target_blank.rb
@@ -1,4 +1,39 @@
 require 'safe_target_blank/version'
+require 'action_view'
+include ActionView::Helpers::UrlHelper
 
-module SafeTargetBlank
+module ActionView
+  module Helpers
+    module UrlHelper
+      alias original_convert_options_to_data_attributes convert_options_to_data_attributes
+
+      def link_to_target_blank?(html_options)
+        html_options['target'] && html_options['target'].to_s.include?('_blank')
+      end
+
+      def link_to_rel_from_html_options(html_options)
+        html_options['rel'].is_a?(Array) ? html_options['rel'].map(&:to_s) : html_options['rel'].to_s.split(' ')
+      end
+
+      def link_to_option_enabled?(html_options, option)
+        html_options.is_a?(Hash) && (html_options.delete(option.to_s) || html_options.delete(option.to_sym))
+      end
+
+      def link_to_target_blank_default_rel(html_options)
+        rel = link_to_rel_from_html_options(html_options)
+        rel.push('noopener') unless link_to_option_enabled?(html_options, 'opener')
+        rel.push('noreferrer') unless link_to_option_enabled?(html_options, 'referrer')
+        rel.empty? ? nil : rel.uniq
+      end
+
+      def convert_options_to_data_attributes(options, html_options)
+        if html_options
+          html_options = html_options.stringify_keys
+          html_options['rel'] = link_to_target_blank_default_rel(html_options) if link_to_target_blank?(html_options)
+        end
+
+        original_convert_options_to_data_attributes(options, html_options)
+      end
+    end
+  end
 end
--- spec/safe_target_blank_spec.rb
+++ spec/safe_target_blank_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe SafeTargetBlank do
   it { expect(SafeTargetBlank::VERSION).not_to be nil }
+
+  describe 'link_to' do
+    it { expect(link_to 'Safe', 'safe.io').to eq "<a href=\"safe.io\">Safe</a>" }
+    it { expect(link_to 'Safe', 'safe.io', target: :_self).to eq "<a target=\"_self\" href=\"safe.io\">Safe</a>" }
+
+    it { expect(link_to 'Safe', 'safe.io', target: '_blank').to eq "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"safe.io\">Safe</a>" }
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank).to eq "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"safe.io\">Safe</a>" }
+
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank, rel: :noopener).to eq "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"safe.io\">Safe</a>" }
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank, rel: :nofollow).to eq "<a target=\"_blank\" rel=\"nofollow noopener noreferrer\" href=\"safe.io\">Safe</a>" }
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank, rel: [:nofollow, :author]).to eq "<a target=\"_blank\" rel=\"nofollow author noopener noreferrer\" href=\"safe.io\">Safe</a>" }
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank, rel: %w(nofollow author)).to eq "<a target=\"_blank\" rel=\"nofollow author noopener noreferrer\" href=\"safe.io\">Safe</a>" }
+
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank, opener: true).to eq "<a target=\"_blank\" rel=\"noreferrer\" href=\"safe.io\">Safe</a>" }
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank, referrer: true).to eq "<a target=\"_blank\" rel=\"noopener\" href=\"safe.io\">Safe</a>" }
+    it { expect(link_to 'Safe', 'safe.io', target: :_blank, opener: true, referrer: true).to eq "<a target=\"_blank\" href=\"safe.io\">Safe</a>" }
+  end
 end
